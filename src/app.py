53c4859ca55a137b450186bde601791138b8f61d
alxwen711 <alxwen711@gmail.com>
2026-02-26 13:07:46 -0800

    cleanup UI function structuring

--- src/app.py
+++ src/app.py
@@ -12,7 +12,7 @@ import pandas as pd
 sales_df = pd.read_csv("data/raw/sales_and_customer_insights.csv", parse_dates=True)
 sales_df["risk_value"] = sales_df["Lifetime_Value"]*sales_df["Churn_Probability"]
 
-# Macro functions for components
+# Isolated components for easier editing
 
 kpi_component = ui.layout_columns(
     ui.layout_columns(
@@ -28,131 +28,139 @@ kpi_component = ui.layout_columns(
     fill=False
 )
 
+main_sidebar = ui.sidebar(
+    ui.input_slider(
+        id="slider_churn",
+        label="Churn Rate",
+        min=0.0,
+        max=1.0,
+        value=[0.0, 1.0],
+    ),
+    ui.input_slider(
+        id="slider_customer",
+        label="Customer Lifetime Value",
+        min=100,
+        max=10000,
+        value=[100, 10000],
+    ),
+    ui.input_slider(
+        id="slider_order",
+        label="Average Order Value",
+        min=20,
+        max=200,
+        value=[20, 200],
+    ),
+    ui.input_slider(
+        id="slider_freq",
+        label="Purchase Frequency",
+        min=1,
+        max=19,
+        value=[1, 19],
+    ),             
+    ui.input_date_range("inDateRange", "Input date"),
+    ui.input_checkbox_group(
+        id="checkbox_group_type",
+        label="Most Common Purchase Type",
+        choices={
+            "Clothing": "Clothing",
+            "Electronics": "Electronics",
+            "Home": "Home",
+            "Sports": "Sports",             
+        },
+        selected=[
+            "Clothing",
+        ],
+    ),
+    ui.input_checkbox_group(
+        id="checkbox_group_region",
+        label="Region",
+        choices={
+            "Asia": "Asia",
+            "Europe": "Europe",
+            "North America": "North America",
+            "South America": "South America",
+        },
+        selected=[
+            "North America",
+        ],
+    ),
+    ui.input_checkbox_group(
+        id="checkbox_group_strategy",
+        label="Retention Strategy",
+        choices={
+            "Discount": "Discount",
+            "Email Campaign": "Email Campaign",
+            "Loyalty Program": "Loyalty Program"
+        },
+        selected=[
+            "Discount",
+            "Email Campaign",
+            "Loyalty Program",
+        ],
+    ),
 
+    ui.input_action_button("action_button", "Apply filter"),
+    open="desktop",
+)
+
+
+# Specialized table for User Story 1
+panel_1 = ui.nav_panel("User Story 1", 
+    ui.layout_columns(
+        ui.input_select(id = "row_dropdown",
+                        label = "Table partition options:",
+                        choices = ["Region","Retention Strategy","Most Frequent Value"]),
+        ui.navset_card_tab( # replace each of these with instances of ui.output_data_frame
+            ui.nav_panel("Customer Lifetime Value", ui.output_data_frame("customer_df")),
+            ui.nav_panel("Value-at-risk", ui.output_data_frame("risk_df")),
+            ui.nav_panel("Order Value", ui.output_data_frame("order_df")),
+            ui.nav_panel("Purchase Frequency", ui.output_data_frame("frequency_df")),
+            id = "multitabtable"
+        ),
+        col_widths = [3,9]
+    ),
+)
+
+# Specialized plot for User Story 2
+panel_2 = ui.nav_panel("User Story 2", 
+    ui.layout_columns(
+        ui.card(
+            ui.card_header("High Churn Risk Scatterplot"),
+            ui.output_image("high_churn_risk"), # placeholder, swap with below for M2
+            #output_widget("high_churn_risk"),
+            full_screen=True,
+        ),
+        col_widths=[12],
+    ),
+)
+
+# Specialized plot for User Story 3
+panel_3 = ui.nav_panel("User Story 3", 
+    ui.layout_columns(
+            ui.card(
+                ui.card_header("Seasonal and Product Type Heatmap"),
+                ui.output_image("heatmap"), # placeholder, swap with below for M2
+                #output_widget("heatmap"),
+                full_screen=True,
+            ),
+            col_widths=[12],
+        ),
+)
 
 # UI
 app_ui = ui.page_fluid(
     ui.tags.style("body { font-size: 0.6em; }"),
     ui.panel_title("Salescope"),
     ui.layout_sidebar(
-        ui.sidebar(
-            ui.input_slider(
-                id="slider_churn",
-                label="Churn Rate",
-                min=0.0,
-                max=1.0,
-                value=[0.0, 1.0],
-            ),
-            ui.input_slider(
-                id="slider_customer",
-                label="Customer Lifetime Value",
-                min=100,
-                max=10000,
-                value=[100, 10000],
-            ),
-            ui.input_slider(
-                id="slider_order",
-                label="Average Order Value",
-                min=20,
-                max=200,
-                value=[20, 200],
-            ),
-            ui.input_slider(
-                id="slider_freq",
-                label="Purchase Frequency",
-                min=1,
-                max=19,
-                value=[1, 19],
-            ),             
-            ui.input_date_range("inDateRange", "Input date"),
-            ui.input_checkbox_group(
-                id="checkbox_group_type",
-                label="Most Common Purchase Type",
-                choices={
-                    "Clothing": "Clothing",
-                    "Electronics": "Electronics",
-                    "Home": "Home",
-                    "Sports": "Sports",             
-                },
-                selected=[
-                    "Clothing",
-                ],
-            ),
-            ui.input_checkbox_group(
-                id="checkbox_group_region",
-                label="Region",
-                choices={
-                    "Asia": "Asia",
-                    "Europe": "Europe",
-                    "North America": "North America",
-                    "South America": "South America",
-                },
-                selected=[
-                    "North America",
-                ],
-            ),
-            ui.input_checkbox_group(
-                id="checkbox_group_strategy",
-                label="Retention Strategy",
-                choices={
-                    "Discount": "Discount",
-                    "Email Campaign": "Email Campaign",
-                    "Loyalty Program": "Loyalty Program"
-                },
-                selected=[
-                    "Discount",
-                    "Email Campaign",
-                    "Loyalty Program",
-                ],
-            ),
-        
-            ui.input_action_button("action_button", "Apply filter"),
-            open="desktop",
-        ),
+        main_sidebar,
         kpi_component,
         ui.navset_bar(
-            ui.nav_panel("User Story 1", 
-                ui.layout_columns(
-                    ui.input_select(id = "row_dropdown",
-                                    label = "Table partition options:",
-                                    choices = ["Region","Retention Strategy","Most Frequent Value"]),
-                    ui.navset_card_tab( # replace each of these with instances of ui.output_data_frame
-                        ui.nav_panel("Customer Lifetime Value", ui.output_data_frame("customer_df")),
-                        ui.nav_panel("Value-at-risk", ui.output_data_frame("risk_df")),
-                        ui.nav_panel("Order Value", ui.output_data_frame("order_df")),
-                        ui.nav_panel("Purchase Frequency", ui.output_data_frame("frequency_df")),
-                        id = "multitabtable"
-                    ),
-                    col_widths = [3,9]
-                ),
-            ),       
-            ui.nav_panel("User Story 2", 
-                ui.layout_columns(
-                    ui.card(
-                        ui.card_header("High Churn Risk Scatterplot"),
-                        ui.output_image("high_churn_risk"), # placeholder, swap with below for M2
-                        #output_widget("high_churn_risk"),
-                        full_screen=True,
-                    ),
-                    col_widths=[12],
-                ),
-            ),
-            ui.nav_panel("User Story 3", 
-                ui.layout_columns(
-                        ui.card(
-                            ui.card_header("Seasonal and Product Type Heatmap"),
-                            ui.output_image("heatmap"), # placeholder, swap with below for M2
-                            #output_widget("heatmap"),
-                            full_screen=True,
-                        ),
-                        col_widths=[12],
-                    ),
-            ),
+            panel_1,       
+            panel_2,
+            panel_3,
             title = "SaleScope"
-        )
-        
-    )
-    
+        )   
+    )    
 )
 
 
@@ -209,7 +217,7 @@ def server(input, output, session):
 
     @render.data_frame
     def risk_df():
-        return create_summary_table(filtered_df(),"Region","risk_value") # still need to compute risk column here
+        return create_summary_table(filtered_df(),"Region","risk_value")
 
     @render.data_frame
     def order_df():
@@ -232,11 +240,6 @@ def server(input, output, session):
     @render.text
     def kpi_count():
         return f"{len(filtered_df()):,}"
-
-    
-    
-
-    
 
 
 # Create app
