3bf7c318e75f87e191aa06167e09c8c5898a3e6b
alxwen711 <alxwen711@gmail.com>
2026-02-26 09:55:20 -0800

    split dashboard components to 3 panels

--- src/app.py
+++ src/app.py
@@ -15,131 +15,320 @@ sales_df["risk_value"] = sales_df["Lifetime_Value"]*sales_df["Churn_Probability"
 # UI
 app_ui = ui.page_fluid(
     ui.tags.style("body { font-size: 0.6em; }"),
-    ui.panel_title("Salescope"),
-    ui.layout_sidebar(
-        ui.sidebar(
-            ui.input_slider(
-                id="slider_churn",
-                label="Churn Rate",
-                min=0.0,
-                max=1.0,
-                value=[0.0, 1.0],
-            ),
-            ui.input_slider(
-                id="slider_customer",
-                label="Customer Lifetime Value",
-                min=5,
-                max=89,
-                value=[5, 89],
-            ),
-            ui.input_slider(
-                id="slider_order",
-                label="Average Order Value",
-                min=20,
-                max=200,
-                value=[20, 200],
-            ),
-            ui.input_slider(
-                id="slider_freq",
-                label="Purchase Frequency",
-                min=1,
-                max=19,
-                value=[1, 19],
-            ),
-            
-            ui.input_date_range("inDateRange", "Input date"),
-
-            ui.input_checkbox_group(
-                id="checkbox_group_type",
-                label="Most Common Purchase Type",
-                choices={
-                    "Clothing": "Clothing",
-                    "Electronics": "Electronics",
-                    "Home": "Home",
-                    "Sports": "Sports",
+    ui.navset_bar(
+        ui.nav_panel("User Story 1", 
+            ui.panel_title("Salescope"),
+            ui.layout_sidebar(
+                ui.sidebar(
+                    ui.input_slider(
+                        id="slider_churn",
+                        label="Churn Rate",
+                        min=0.0,
+                        max=1.0,
+                        value=[0.0, 1.0],
+                    ),
+                    ui.input_slider(
+                        id="slider_customer",
+                        label="Customer Lifetime Value",
+                        min=5,
+                        max=89,
+                        value=[5, 89],
+                    ),
+                    ui.input_slider(
+                        id="slider_order",
+                        label="Average Order Value",
+                        min=20,
+                        max=200,
+                        value=[20, 200],
+                    ),
+                    ui.input_slider(
+                        id="slider_freq",
+                        label="Purchase Frequency",
+                        min=1,
+                        max=19,
+                        value=[1, 19],
+                    ),             
+                    ui.input_date_range("inDateRange", "Input date"),
+                    ui.input_checkbox_group(
+                        id="checkbox_group_type",
+                        label="Most Common Purchase Type",
+                        choices={
+                            "Clothing": "Clothing",
+                            "Electronics": "Electronics",
+                            "Home": "Home",
+                            "Sports": "Sports",             
+                        },
+                        selected=[
+                            "Clothing",
+                        ],
+                    ),
+                    ui.input_checkbox_group(
+                        id="checkbox_group_region",
+                        label="Region",
+                        choices={
+                            "Asia": "Asia",
+                            "Europe": "Europe",
+                            "North America": "North America",
+                            "South America": "South America",
+                        },
+                        selected=[
+                            "North America",
+                        ],
+                    ),
+                    ui.input_checkbox_group(
+                        id="checkbox_group_strategy",
+                        label="Retention Strategy",
+                        choices={
+                            "Discount": "Discount",
+                            "Email Campaign": "Email Campaign",
+                            "Loyalty Program": "Loyalty Program"
+                        },
+                        selected=[
+                            "Discount",
+                            "Email Campaign",
+                            "Loyalty Program",
+                        ],
+                    ),
+                
+                    ui.input_action_button("action_button", "Apply filter"),
+                    open="desktop",
+                ),
+                ui.layout_columns(
+                    ui.layout_columns(
+                        ui.value_box("Average Lifetime Value", "5432.86"),
+                        ui.value_box("Average Churn Rate", "0.727"),
+                        ui.value_box("Average Value-At-Risk", "1234.65"),
+                        ui.value_box("Average Days Per Purchase", "5.315"),
+                        col_widths = (6,6,6,6)
+                    ),
                     
-                },
-                selected=[
-                    "Clothing",
-                ],
-            ),
-
-            ui.input_checkbox_group(
-                id="checkbox_group_region",
-                label="Region",
-                choices={
-                    "Asia": "Asia",
-                    "Europe": "Europe",
-                    "North America": "North America",
-                    "South America": "South America",
-                },
-                selected=[
-                    "North America",
-                ],
-            ),
-
-            ui.input_checkbox_group(
-                id="checkbox_group_strategy",
-                label="Retention Strategy",
-                choices={
-                    "Discount": "Discount",
-                    "Email Campaign": "Email Campaign",
-                    "Loyalty Program": "Loyalty Program"
-                },
-                selected=[
-                    "Discount",
-                    "Email Campaign",
-                    "Loyalty Program",
-                ],
-            ),
-            
-            ui.input_action_button("action_button", "Apply filter"),
-            open="desktop",
+                    ui.value_box("Count of Datapoints", "1234"),
+                    col_widths = (8,4), # 12 part ratio
+                    # row_heights= (1,2), # direct ratio
+                    fill=False
+                ),
+                ui.layout_columns(
+                    ui.input_select(id = "row_dropdown",
+                                    label = "Table partition options:",
+                                    choices = ["Region","Retention Strategy","Most Frequent Value"]),
+                    ui.navset_card_tab( # replace each of these with instances of ui.output_data_frame
+                        ui.nav_panel("Customer Lifetime Value", ui.output_data_frame("customer_df")),
+                        ui.nav_panel("Value-at-risk", ui.output_data_frame("risk_df")),
+                        ui.nav_panel("Order Value", ui.output_data_frame("order_df")),
+                        ui.nav_panel("Purchase Frequency", ui.output_data_frame("frequency_df")),
+                        id = "multitabtable"
+                    ),
+                    col_widths = [3,9]
+                ),
+            )
         ),
-        ui.layout_columns(
-            ui.layout_columns(
-                ui.value_box("Average Lifetime Value", "5432.86"),
-                ui.value_box("Average Churn Rate", "0.727"),
-                ui.value_box("Average Value-At-Risk", "1234.65"),
-                ui.value_box("Average Days Per Purchase", "5.315"),
-                col_widths = (6,6,6,6)
-            ),
-            
-            ui.value_box("Count of Datapoints", "1234"),
-            col_widths = (8,4), # 12 part ratio
-            # row_heights= (1,2), # direct ratio
-            fill=False
+        ui.nav_panel("User Story 2", 
+            ui.panel_title("Salescope"),
+            ui.layout_sidebar(
+                ui.sidebar(
+                    ui.input_slider(
+                        id="slider_churn",
+                        label="Churn Rate",
+                        min=0.0,
+                        max=1.0,
+                        value=[0.0, 1.0],
+                    ),
+                    ui.input_slider(
+                        id="slider_customer",
+                        label="Customer Lifetime Value",
+                        min=5,
+                        max=89,
+                        value=[5, 89],
+                    ),
+                    ui.input_slider(
+                        id="slider_order",
+                        label="Average Order Value",
+                        min=20,
+                        max=200,
+                        value=[20, 200],
+                    ),
+                    ui.input_slider(
+                        id="slider_freq",
+                        label="Purchase Frequency",
+                        min=1,
+                        max=19,
+                        value=[1, 19],
+                    ),             
+                    ui.input_date_range("inDateRange", "Input date"),
+                    ui.input_checkbox_group(
+                        id="checkbox_group_type",
+                        label="Most Common Purchase Type",
+                        choices={
+                            "Clothing": "Clothing",
+                            "Electronics": "Electronics",
+                            "Home": "Home",
+                            "Sports": "Sports",             
+                        },
+                        selected=[
+                            "Clothing",
+                        ],
+                    ),
+                    ui.input_checkbox_group(
+                        id="checkbox_group_region",
+                        label="Region",
+                        choices={
+                            "Asia": "Asia",
+                            "Europe": "Europe",
+                            "North America": "North America",
+                            "South America": "South America",
+                        },
+                        selected=[
+                            "North America",
+                        ],
+                    ),
+                    ui.input_checkbox_group(
+                        id="checkbox_group_strategy",
+                        label="Retention Strategy",
+                        choices={
+                            "Discount": "Discount",
+                            "Email Campaign": "Email Campaign",
+                            "Loyalty Program": "Loyalty Program"
+                        },
+                        selected=[
+                            "Discount",
+                            "Email Campaign",
+                            "Loyalty Program",
+                        ],
+                    ),
+                
+                    ui.input_action_button("action_button", "Apply filter"),
+                    open="desktop",
+                ),
+                ui.layout_columns(
+                    ui.layout_columns(
+                        ui.value_box("Average Lifetime Value", "5432.86"),
+                        ui.value_box("Average Churn Rate", "0.727"),
+                        ui.value_box("Average Value-At-Risk", "1234.65"),
+                        ui.value_box("Average Days Per Purchase", "5.315"),
+                        col_widths = (6,6,6,6)
+                    ),
+                    
+                    ui.value_box("Count of Datapoints", "1234"),
+                    col_widths = (8,4), # 12 part ratio
+                    # row_heights= (1,2), # direct ratio
+                    fill=False
+                ),
+                ui.layout_columns(
+                    ui.card(
+                        ui.card_header("High Churn Risk Scatterplot"),
+                        ui.output_image("high_churn_risk"), # placeholder, swap with below for M2
+                        #output_widget("high_churn_risk"),
+                        full_screen=True,
+                    ),
+                    col_widths=[12],
+                ),
+            )
         ),
-       
-        ui.layout_columns(
-            ui.card(
-                ui.card_header("High Churn Risk Scatterplot"),
-                ui.output_image("high_churn_risk"), # placeholder, swap with below for M2
-                #output_widget("high_churn_risk"),
-                full_screen=True,
-            ),
-            ui.card(
-                ui.card_header("Seasonal and Product Type Heatmap"),
-                ui.output_image("heatmap"), # placeholder, swap with below for M2
-                #output_widget("heatmap"),
-                full_screen=True,
-            ),
-            col_widths=[6, 6],
+        ui.nav_panel("User Story 3", 
+            ui.panel_title("Salescope"),
+            ui.layout_sidebar(
+                ui.sidebar(
+                    ui.input_slider(
+                        id="slider_churn",
+                        label="Churn Rate",
+                        min=0.0,
+                        max=1.0,
+                        value=[0.0, 1.0],
+                    ),
+                    ui.input_slider(
+                        id="slider_customer",
+                        label="Customer Lifetime Value",
+                        min=5,
+                        max=89,
+                        value=[5, 89],
+                    ),
+                    ui.input_slider(
+                        id="slider_order",
+                        label="Average Order Value",
+                        min=20,
+                        max=200,
+                        value=[20, 200],
+                    ),
+                    ui.input_slider(
+                        id="slider_freq",
+                        label="Purchase Frequency",
+                        min=1,
+                        max=19,
+                        value=[1, 19],
+                    ),             
+                    ui.input_date_range("inDateRange", "Input date"),
+                    ui.input_checkbox_group(
+                        id="checkbox_group_type",
+                        label="Most Common Purchase Type",
+                        choices={
+                            "Clothing": "Clothing",
+                            "Electronics": "Electronics",
+                            "Home": "Home",
+                            "Sports": "Sports",             
+                        },
+                        selected=[
+                            "Clothing",
+                        ],
+                    ),
+                    ui.input_checkbox_group(
+                        id="checkbox_group_region",
+                        label="Region",
+                        choices={
+                            "Asia": "Asia",
+                            "Europe": "Europe",
+                            "North America": "North America",
+                            "South America": "South America",
+                        },
+                        selected=[
+                            "North America",
+                        ],
+                    ),
+                    ui.input_checkbox_group(
+                        id="checkbox_group_strategy",
+                        label="Retention Strategy",
+                        choices={
+                            "Discount": "Discount",
+                            "Email Campaign": "Email Campaign",
+                            "Loyalty Program": "Loyalty Program"
+                        },
+                        selected=[
+                            "Discount",
+                            "Email Campaign",
+                            "Loyalty Program",
+                        ],
+                    ),
+                
+                    ui.input_action_button("action_button", "Apply filter"),
+                    open="desktop",
+                ),
+                ui.layout_columns(
+                    ui.layout_columns(
+                        ui.value_box("Average Lifetime Value", "5432.86"),
+                        ui.value_box("Average Churn Rate", "0.727"),
+                        ui.value_box("Average Value-At-Risk", "1234.65"),
+                        ui.value_box("Average Days Per Purchase", "5.315"),
+                        col_widths = (6,6,6,6)
+                    ),
+                    
+                    ui.value_box("Count of Datapoints", "1234"),
+                    col_widths = (8,4), # 12 part ratio
+                    # row_heights= (1,2), # direct ratio
+                    fill=False
+                ),
+                ui.layout_columns(
+                    ui.card(
+                        ui.card_header("Seasonal and Product Type Heatmap"),
+                        ui.output_image("heatmap"), # placeholder, swap with below for M2
+                        #output_widget("heatmap"),
+                        full_screen=True,
+                    ),
+                    col_widths=[12],
+                ),
+            )
         ),
-        ui.layout_columns(
-            ui.input_select(id = "row_dropdown",
-                            label = "Table partition options:",
-                            choices = ["Region","Retention Strategy","Most Frequent Value"]),
-            ui.navset_card_tab( # replace each of these with instances of ui.output_data_frame
-                ui.nav_panel("Customer Lifetime Value", ui.output_data_frame("customer_df")),
-                ui.nav_panel("Value-at-risk", ui.output_data_frame("risk_df")),
-                ui.nav_panel("Order Value", ui.output_data_frame("order_df")),
-                ui.nav_panel("Purchase Frequency", ui.output_data_frame("frequency_df")),
-                id = "multitabtable"
-            ),
-            col_widths = [3,9]
-        ),
-    ),
+        title = "SaleScope"
+    )
+    
 )
 
 def create_summary_table(df,grouping,feature):
