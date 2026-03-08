da4eae3f13978212f876d1e062ac710606bb8672
Raghav Gupta <raghav9048@gmail.com>
2026-03-07 23:51:57 -0800

    adding avg order value for ai dashboard

--- src/app.py
+++ src/app.py
@@ -608,7 +608,7 @@ def server(input, output, session):
     def order_df():
         mapping = {"Region": "Region", "Retention Strategy": "Retention_Strategy", "Most Frequent Value": "Most_Frequent_Category"}
         group = mapping[input.row_dropdown()]
-        return create_summary_table(filtered_df(), group, "Average_Order_Value")
+        return create_summary_table(dashboard_df(), group, "Average_Order_Value")
 
     @render.data_frame
     def frequency_df():
